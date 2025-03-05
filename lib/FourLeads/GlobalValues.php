b5824b91ba62493a1b2a576bf1f254c0ed638c11
Nico Bombek <github.bd@4leads.de>
2025-03-05 16:49:15 +0100

    added global value helper class

---
+++ lib/FourLeads/GlobalValues.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FourLeads;
+
+/**
+ * Helper class with constants.
+ * Class GlobalValues.
+ * @package FourLeads
+ */
+class GlobalValues
+{
+    const TYPE_TEXT = 'text';
+    const TYPE_NUMERIC = 'numeric';
+    const TYPE_NUMERIC_SUM = 'numeric-sum';
+    const TYPE_DATETIME = 'datetime';
+
+    const LOCK_MODE_DEFAULT = 0;
+    const LOCK_MODE_IGNORE = 1;
+    const LOCK_MODE_FORCE = 2;
+}
